550252573b8e12cd2a88295fea7bc64e48aba22b
Swift <swiftshineexists@gmail.com>
2026-05-20 17:39:57 -0500

    update dependency versions + colored text

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use clap::{Parser, ValueEnum};
-use gfarch::gfarch;
 use anyhow::Result;
-use std::{fs, path::Path};
-use colored::*;
+use clap::{Parser, ValueEnum};
+use crossterm::style::Stylize;
+use gfarch::gfarch;
 use std::num::ParseIntError;
+use std::{fs, path::Path};
 
 #[derive(Debug, Clone, ValueEnum)]
 enum ArchiveVersion {
@@ -17,13 +17,13 @@ enum ArchiveVersion {
 #[derive(Debug, Clone, ValueEnum)]
 enum ToolUsage {
     Extract, // To extract an archive's contents
-    Archive // To create an archive
+    Archive, // To create an archive
 }
 
 #[derive(Debug, Clone, ValueEnum)]
 enum CompressionType {
     BPE,
-    LZ10
+    LZ10,
 }
 
 #[derive(Parser, Debug)]
@@ -67,7 +67,7 @@ fn main() -> Result<()> {
             // extract file contents
             let archive = fs::read(&args.input)?;
             let extracted = gfarch::extract(&archive)?;
-            
+
             let folder_name = if args.output.is_some() {
                 &args.output.unwrap()
             } else {
@@ -86,62 +86,88 @@ fn main() -> Result<()> {
             for file in extracted {
                 let result = fs::write(folder_name.to_string() + &file.0, file.1);
                 if let Err(e) = result {
-                    eprintln!("{} '{}' {}. Error: {}", "Failed to write the contents of".yellow(), file.0, "to disk.".yellow(), e);
+                    eprintln!(
+                        "{} '{}' {}. Error: {}",
+                        "Failed to write the contents of".yellow(),
+                        file.0,
+                        "to disk.".yellow(),
+                        e
+                    );
                     num_errors += 1;
                 }
             }
 
             if num_errors != 0 {
                 if num_errors != num_files {
-                    println!("{} {} {}", "Finished with".yellow(), num_errors, "errors.".yellow());
+                    println!(
+                        "{} {} {}",
+                        "Finished with".yellow(),
+                        num_errors,
+                        "errors.".yellow()
+                    );
                 } else {
                     println!("{}", "Failed.".red());
                     std::process::exit(1);
                 }
             }
-        },
+        }
 
         ToolUsage::Archive => {
             // validate input
             if !fs::exists(&args.input)? {
-                eprintln!("{} '{}' {}", "The folder".red(), args.input, "does not exist.".red());
+                eprintln!(
+                    "{} '{}' {}",
+                    "The folder".red(),
+                    args.input,
+                    "does not exist.".red()
+                );
                 std::process::exit(1);
             }
-            
-            
+
             if args.archive_version.is_none() {
                 eprintln!("{}", "An archive version is required.".red());
                 std::process::exit(1);
             }
-            
+
             let archive_version = args.archive_version.unwrap();
 
             if args.compression_type.is_none() {
                 eprintln!("{}", "A compression type is required.".red());
                 std::process::exit(1);
             }
-            
+
             let compression_type = args.compression_type.unwrap();
-            
+
             // read file contents
             let mut files: Vec<(String, Vec<u8>)> = Vec::new();
-            
+
             let mut num_errors = 0;
             for entry in fs::read_dir(&args.input)? {
-                let entry = if entry.is_ok() { entry.unwrap() } else {
+                let entry = if entry.is_ok() {
+                    entry.unwrap()
+                } else {
                     num_errors += 1;
-                    continue; 
+                    continue;
                 };
-                
+
                 let path = entry.path();
-                let filename = path.file_name().unwrap_or_default().to_string_lossy().to_string();
+                let filename = path
+                    .file_name()
+                    .unwrap_or_default()
+                    .to_string_lossy()
+                    .to_string();
                 let contents = fs::read(path)?;
-                
+
                 files.push((filename, contents));
             }
-            
+
             if files.len() == 0 {
-                eprintln!("{} '{}' {}", "The folder".red(), &args.input, "is empty.".red());
+                eprintln!(
+                    "{} '{}' {}",
+                    "The folder".red(),
+                    &args.input,
+                    "is empty.".red()
+                );
                 std::process::exit(1);
             }
 
@@ -150,10 +176,10 @@ fn main() -> Result<()> {
                 CompressionType::BPE => gfarch::CompressionType::BPE,
                 CompressionType::LZ10 => gfarch::CompressionType::LZ10,
             };
-            
+
             let archive_version = match archive_version {
-                ArchiveVersion::V2 =>   gfarch::Version::V2,
-                ArchiveVersion::V3 =>   gfarch::Version::V3,
+                ArchiveVersion::V2 => gfarch::Version::V2,
+                ArchiveVersion::V3 => gfarch::Version::V3,
                 ArchiveVersion::V3_1 => gfarch::Version::V3_1,
             };
 
@@ -165,36 +191,37 @@ fn main() -> Result<()> {
             };
 
             // create archive
-            let archive = gfarch::pack_from_files(
-                &files,
-                archive_version,
-                compression_type,
-                offset
-            );
-            
+            let archive =
+                gfarch::pack_from_files(&files, archive_version, compression_type, offset);
+
             // write archive
-            
+
             let output_name = match args.output {
                 Some(ref name) => {
                     // if the name ends with ".gfa", remove it
                     if name.ends_with(".gfa") {
-                        &name[..name.len() - 4]  // remove the last 4 characters
+                        &name[..name.len() - 4] // remove the last 4 characters
                     } else {
                         name
                     }
                 }
                 None => &args.input,
-            }.to_string();
+            }
+            .to_string();
 
-            
             fs::write(output_name + ".gfa", archive)?;
 
             if num_errors != 0 {
-                println!("{} {} {}", "Failed to store".red(), num_errors, "in the archive.".red());
+                println!(
+                    "{} {} {}",
+                    "Failed to store".red(),
+                    num_errors,
+                    "in the archive.".red()
+                );
             }
         }
     }
-    
+
     println!("{}", "Done.".green());
     Ok(())
 }
